1d75ee212ba54d738f1bec01bbe521a0dfc73cb9
lauritula <mlaura.tula@gmail.com>
2015-04-30 22:00:57 -0300

    espacio

---
+++ Calculadora/src/ar/edu/unlam/tallerweb/calculadora/CalculadoraException.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.tallerweb.calculadora;
+
+public class CalculadoraException extends Exception {
+
+	public CalculadoraException(String string) {
+		
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
